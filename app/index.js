a6d159b9f23dd1b10e8d294443fbcce9b6ffe1c7
joestephens <joe.stephens@live.co.uk>
2016-08-28 18:59:18 +0100

    initial react setup and routes

---
+++ app/index.js
@@ -0,0 +1,28 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
+var Raven = require('raven-js');
+
+var sentryKey = ''
+var sentryApp = ''
+var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp
+
+var _APP_INFO = {
+  name: 'GitHub Battle',
+  branch: 'video4',
+  version: '1.0'
+}
+
+Raven.config(sentryURL, {
+  release: _APP_INFO.version,
+  tags: {
+    branch: _APP_INFO.branch
+  }
+}).install()
+
+console.log(window.thing.nope)
+
+ReactDOM.render(
+  routes,
+  document.getElementById('app')
+);
